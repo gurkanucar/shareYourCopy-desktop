f1c604b4af05e0f64ca224222674efbdb8fab995
Gurkan UCAR <gurkan.ucar@bulutmd.com>
2020-09-09 17:12:10 +0300

    initial commit

---
+++ Share Your Copy/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Share_Your_Copy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 390);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 66);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Server";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(188, 390);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 66);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop Server";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 268);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(500, 88);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(9, 239);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Logs";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(9, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Copy Data";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(15, 42);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(497, 184);
+            this.listBox1.TabIndex = 7;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(405, 431);
+            this.textBox1.MaxLength = 4;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(67, 22);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(365, 393);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "İp:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(401, 393);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Logs";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(347, 431);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Port:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 468);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Share Your Copy";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Share Your Copy/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+
+namespace Share_Your_Copy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Thread thr;
+
+        IPEndPoint endpoint;
+        int port = 7001;
+        Socket sck;
+        static bool continueVal = true;
+        IPAddress ipAddress;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    label1.Text = ip.ToString();
+                    ipAddress = ip;
+                }
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.ToString().Length < 5 && textBox1.Text.ToString().Length > 0)
+            {
+                port = Convert.ToInt32(textBox1.Text.ToString());
+                Control.CheckForIllegalCrossThreadCalls = false;
+                if (thr == null)
+                {
+                    thr = new Thread(listen);
+                    thr.Start();
+                }
+                else
+                {
+                    continueVal = true;
+                }
+            }
+            else
+            {
+                MessageBox.Show("You must write port!");
+            }
+        }
+
+        private void listen()
+        {
+            try
+            {
+                int tempPort = port;
+                byte[] data = new byte[1024];
+                endpoint = new IPEndPoint(ipAddress, port);
+                sck = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                Console.WriteLine("Server started at => " + ipAddress + ":" + port + "...");
+                sck.Bind(endpoint);
+                richTextBox2.Text = "Server started at => " + ipAddress + ":" + port + "...\n" + richTextBox2.Text.ToString();
+
+                while (true)
+                {
+
+                    if (port != tempPort)
+                    {
+                        sck.Dispose();
+                        endpoint = new IPEndPoint(ipAddress, port);
+                        sck = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                        Console.WriteLine("Server started at => " + ipAddress + ":" + port + "...");
+                        richTextBox2.Text = "Server started at => " + ipAddress + ":" + port + "...\n" + richTextBox2.Text.ToString();
+                        sck.Bind(endpoint);
+                    }
+
+                    Console.WriteLine("i am listening");
+                    EndPoint remote = (EndPoint)endpoint;
+                    int recv = sck.ReceiveFrom(data, ref remote);
+                    string listenData = Encoding.ASCII.GetString(data, 0, recv).ToString();
+                    if (continueVal == true)
+                    {
+                        String prefix = listenData.Split(new string[] { "/#/" }, StringSplitOptions.None)[0].ToString();
+                        String title = listenData.Split(new string[] { "/#/" }, StringSplitOptions.None)[1].ToString();
+                        String message = listenData.Split(new string[] { "/#/" }, StringSplitOptions.None)[2].ToString();
+                        if (prefix == "LOG")
+                        {
+                            richTextBox2.Text = "LOG:" + ipAddress + ":" + port + " => "+title +": "+message+ "...\n" + richTextBox2.Text.ToString();
+
+                        }
+                        else
+                        {
+                            listBox1.Items.Insert(0, title + ": " + message);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Server stoped...");
+                        richTextBox2.Text += "Server stoped...\n";
+                    }
+                }
+
+            }
+
+
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Error: " + ex);
+                richTextBox2.Text += "Error:" + ex + "...\n";
+
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(Environment.ExitCode);
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            continueVal = false;
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show(listBox1.SelectedItem + "");
+        }
+    }
+}
